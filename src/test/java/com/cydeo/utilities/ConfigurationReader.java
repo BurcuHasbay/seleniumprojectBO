2f33e132a92afb0d132f18f38e83b58f79b4caa2
burcuhasbay <burcu.hasbay@gmail.com>
2023-08-29 15:14:30 +0300

    Creating Configuration Method in the utilities

--- src/test/java/com/cydeo/utilities/ConfigurationReader.java
+++ src/test/java/com/cydeo/utilities/ConfigurationReader.java
@@ -5,27 +5,26 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class ConfigurationReader {
-  private static Properties properties = new Properties();
 
-static {
-
-    //2-We need to open the file in java memory: FileInputStream
+    private static Properties properties = new Properties();
 
 
+    static {
 
-    //3-Load the properties object using FileInputStream object
-    try {
+        try {
+
+            FileInputStream file = new FileInputStream("configuration.properties")
+            properties.load(file);
+
+            //It is better to close it if it is not in use.
+            file.close();
 
 
-        FileInputStream file = new FileInputStream("configuration.properties");
-        properties.load(file);
-        //close the file-It is better to close it if ir is not in used.
-        file.close();
+        } catch (IOException e) {
+            System.out.println("Something happened in the ConfigurationReader class ");
+            throw new RuntimeException(e);
+        }
 
-    } catch (IOException e) {
-        System.out.println(" Something happened in the ConfigurationReader class. ");;
     }
-
-}
 
 }
