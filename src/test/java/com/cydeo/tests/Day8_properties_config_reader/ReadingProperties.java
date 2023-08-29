a9cbf4a66f645ab4325e5f9a21c7b13ef327dbe9
burcuhasbay <burcu.hasbay@gmail.com>
2023-08-29 14:08:57 +0300

    New Task For Reading Properties

---
+++ src/test/java/com/cydeo/tests/Day8_properties_config_reader/ReadingProperties.java
@@ -0,0 +1,31 @@
+package com.cydeo.tests.Day8_properties_config_reader;
+
+import org.testng.annotations.Test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ReadingProperties {
+
+    @Test
+    public void readingFrom_Properties_Test() throws IOException {
+
+        //1-Create the object of Properties
+        Properties properties = new Properties();
+
+
+        //2-We need to open the file in java memory: FileInputStream
+
+        FileInputStream file = new FileInputStream("configuration.properties");
+
+        //3-Load the properties object using FileInputStream object
+        properties.load(file);
+
+        //4-We can get any "value" now by passing "Key"
+
+        System.out.println("properties.getProperty(\"browser\") = " + properties.getProperty("browser"));
+
+    }
+
+}
