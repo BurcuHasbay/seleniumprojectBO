b3465139d829b75db92b92fd5bf4c9fbd9d1d3cf
burcuhasbay <burcu.hasbay@gmail.com>
2023-08-30 20:00:02 +0300

    Adding commonly used steps in the TestBase class

--- src/test/java/com/cydeo/tests/Base/TestBase.java
+++ src/test/java/com/cydeo/tests/Base/TestBase.java
@@ -1,4 +1,21 @@
 package com.cydeo.tests.Base;
 
-public class TestBase {
+import com.cydeo.utilities.WebDriver_Factory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+public abstract class TestBase {
+
+    WebDriver driver;
+    @BeforeMethod
+    public void setUpMethod() {
+        driver = WebDriver_Factory.getDriver("Chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    }
+
+
 }
