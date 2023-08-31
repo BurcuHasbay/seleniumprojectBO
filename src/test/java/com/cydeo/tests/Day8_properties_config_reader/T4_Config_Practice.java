1b3690da8c9f9f0a90738d0c3673b2f1a2fee2d1
burcuhasbay <burcu.hasbay@gmail.com>
2023-08-31 16:21:43 +0300

    Using the Driver in the task for the first Time

--- src/test/java/com/cydeo/tests/Day8_properties_config_reader/T4_Config_Practice.java
+++ src/test/java/com/cydeo/tests/Day8_properties_config_reader/T4_Config_Practice.java
@@ -1,48 +1,19 @@
 package com.cydeo.tests.Day8_properties_config_reader;
 
-import com.cydeo.utilities.ConfigurationReader;
+import com.cydeo.utilities.Driver;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import java.util.concurrent.TimeUnit;
 
 public class  T4_Config_Practice {
 
-    WebDriver driver;
-    @BeforeMethod
-    public void setUpMethod(){
-//We are getting the browserType dynamically from our configuration.properties file
-        String browserType = ConfigurationReader.getProperty("browser");
-
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get("https://google.com");
-
-    }
 
     @Test
-    public void googleSearch_Test(){
+    public void googleSearch_Tes(){
+        Driver.getDriver().get("https://google.com");
 
-        //3- Write “apple” in search box 4- Verify title:
-//Expected: apple - Google Search
-
-        WebElement googleSearchBox = driver.findElement(By.xpath("//textarea[@name ='q']"));
-
-        googleSearchBox.sendKeys("apple"+ Keys.ENTER);
-
-        String expectedTitle = "apple - Google'da Ara";
-        String actualTitle = driver.getTitle();
-
-        Assert.assertEquals(actualTitle,expectedTitle);
-
-
-
-
+    //Write "apple" on the search box
+        WebElement googleSearchBox = Driver.getDriver().findElement(By.xpath("//textarea[@name='q']"));
     }
 
 
