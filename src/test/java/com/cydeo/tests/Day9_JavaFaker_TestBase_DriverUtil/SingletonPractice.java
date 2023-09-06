fd534ddee536495d71d8a31bfe243ca834b4c94e
burcuhasbay <burcu.hasbay@gmail.com>
2023-09-06 13:02:27 +0300

    The Practice of Singleton method

--- src/test/java/com/cydeo/tests/Day9_JavaFaker_TestBase_DriverUtil/SingletonPractice.java
+++ src/test/java/com/cydeo/tests/Day9_JavaFaker_TestBase_DriverUtil/SingletonPractice.java
@@ -7,6 +7,10 @@ public class SingletonPractice {
     @Test
     public void singleton_understand_test1(){
 
+        String str1= Singleton.getWord();
+
+        System.out.println("str1 = " + str1);
+
     }
 
 
