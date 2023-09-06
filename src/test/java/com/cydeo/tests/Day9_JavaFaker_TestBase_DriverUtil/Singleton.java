f5b67e571f96446ea8a0cf41d63fd09b697a4bd2
burcuhasbay <burcu.hasbay@gmail.com>
2023-09-06 12:49:19 +0300

    Singleton

--- src/test/java/com/cydeo/tests/Day9_JavaFaker_TestBase_DriverUtil/Singleton.java
+++ src/test/java/com/cydeo/tests/Day9_JavaFaker_TestBase_DriverUtil/Singleton.java
@@ -10,7 +10,14 @@ public class Singleton {
     //create private static String
     private static String word;
 
+    //Creating getter method. This utility method will return the "word" in the way we want to return
+    public static String getWord(){
+        if (word ==null){
+            System.out.println("First time call. Word object is null. " + " Assigning value to it now!");
 
+            word="something";
+        }
+    }
 
 
 }
