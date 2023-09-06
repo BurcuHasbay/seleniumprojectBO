a56e353e6115542e46be53ca9b2dc7628e36e9f7
burcuhasbay <burcu.hasbay@gmail.com>
2023-09-06 13:00:59 +0300

    The Practice of Singleton method

---
+++ src/test/java/com/cydeo/tests/Day9_JavaFaker_TestBase_DriverUtil/SingletonPractice.java
@@ -0,0 +1,13 @@
+package com.cydeo.tests.Day9_JavaFaker_TestBase_DriverUtil;
+
+import org.testng.annotations.Test;
+
+public class SingletonPractice {
+
+    @Test
+    public void singleton_understand_test1(){
+
+    }
+
+
+}
