d22cd78adfe9cfca4667e5e7082f7e6f6a3cdf7f
burcuhasbay <burcu.hasbay@gmail.com>
2023-09-06 12:46:10 +0300

    Using the Driver in the task for the first Time

---
+++ src/test/java/com/cydeo/tests/Day9_JavaFaker_TestBase_DriverUtil/Singleton.java
@@ -0,0 +1,16 @@
+package com.cydeo.tests.Day9_JavaFaker_TestBase_DriverUtil;
+
+public class Singleton {
+
+
+    //Create private constructor
+    private Singleton(){
+    }
+
+    //create private static String
+    private static String word;
+
+
+
+
+}
