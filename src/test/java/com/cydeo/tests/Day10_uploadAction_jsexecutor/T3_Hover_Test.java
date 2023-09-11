b2e94952db0ae346b28300e80559452cbadb4d60
burcuhasbay <burcu.hasbay@gmail.com>
2023-09-11 20:53:31 +0300

    Hovering Test

--- src/test/java/com/cydeo/tests/Day10_uploadAction_jsexecutor/T3_Hover_Test.java
+++ src/test/java/com/cydeo/tests/Day10_uploadAction_jsexecutor/T3_Hover_Test.java
@@ -1,8 +1,11 @@
 package com.cydeo.tests.Day10_uploadAction_jsexecutor;
 
+import com.cydeo.utilities.BrowserUtils;
 import com.cydeo.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class T3_Hover_Test {
@@ -24,20 +27,34 @@ public class T3_Hover_Test {
         WebElement user3 = Driver.getDriver().findElement(By.xpath("//h5[text()='name: user3']"));
 
 
+        //2. Hover over to first image
+        Actions actions = new Actions(Driver.getDriver());
+        BrowserUtils.sleep(2000);
+        actions.moveToElement(img1).perform();
+
+        //3. Assert:
+        //a. “name: user1” is displayed
+        Assert.assertTrue(user1.isDisplayed());
+
+
+
+
+
+
+
 
 
     }
-    //2. Hover over to first image
-    //3. Assert:
-    //a. “name: user1” is displayed
-    //b. “view profile” is displayed
+
+
+
     //4. Hover over to second image
     //5. Assert:
     //a. “name: user2” is displayed
-    //b. “view profile” is displayed //
+
     //6. Hover over to third image
     //7. Confirm:
-    //a. “name: user3” is displayed b. “view profile” is displayed
+    //a. “name: user3” is displayed
 
 
 
