3daeee10c2c62cd5f36f4e175af77086ef266800
burcuhasbay <burcu.hasbay@gmail.com>
2023-09-11 18:16:44 +0300

    Upload Practice

--- src/test/java/com/cydeo/tests/Day10_uploadAction_jsexecutor/T2_Upload_Practice.java
+++ src/test/java/com/cydeo/tests/Day10_uploadAction_jsexecutor/T2_Upload_Practice.java
@@ -1,6 +1,8 @@
 package com.cydeo.tests.Day10_uploadAction_jsexecutor;
 
 import com.cydeo.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 public class T2_Upload_Practice {
@@ -17,6 +19,9 @@ public class T2_Upload_Practice {
        // 2. Find some small file from your computer, and get the path of it. 3. Upload the file.
         String path="/Users/burcuhasbay/Desktop/HOMEWORK 2PDF.pdf";
 
+
+        //3. Upload the file.
+        WebElement uploadButton = Driver.getDriver().findElement(By.xpath("//input[@class='button']"));
 
        // 4. Assert:
        // -File uploaded text is displayed on the page
