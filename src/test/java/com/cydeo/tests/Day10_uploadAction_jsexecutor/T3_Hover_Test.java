de27ac9a488ecfc2b97a8396dcf304d641f521ce
burcuhasbay <burcu.hasbay@gmail.com>
2023-09-11 20:34:02 +0300

    Hovering Test

---
+++ src/test/java/com/cydeo/tests/Day10_uploadAction_jsexecutor/T3_Hover_Test.java
@@ -0,0 +1,31 @@
+package com.cydeo.tests.Day10_uploadAction_jsexecutor;
+
+import com.cydeo.utilities.Driver;
+import org.testng.annotations.Test;
+
+public class T3_Hover_Test {
+
+  //  1. Go to https://practice.cydeo.com/hovers
+    @Test
+    public void hover_Test(){
+
+        Driver.getDriver().get("https://practice.cydeo.com/hovers");
+
+
+
+    }
+    //2. Hover over to first image
+    //        3. Assert:
+    //a. “name: user1” is displayed
+    //b. “view profile” is displayed 4. Hover over to second image
+             //5. Assert:
+    //a. “name: user2” is displayed
+    //b. “view profile” is displayed 6. Hover over to third image
+//7. Confirm:
+    //a. “name: user3” is displayed b. “view profile” is displayed
+
+
+
+
+
+}
