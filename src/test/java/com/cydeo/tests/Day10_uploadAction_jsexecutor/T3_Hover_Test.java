a93d35a2262c49c4b288dc24e46c558decec570f
burcuhasbay <burcu.hasbay@gmail.com>
2023-09-11 20:38:05 +0300

    Hovering Test

--- src/test/java/com/cydeo/tests/Day10_uploadAction_jsexecutor/T3_Hover_Test.java
+++ src/test/java/com/cydeo/tests/Day10_uploadAction_jsexecutor/T3_Hover_Test.java
@@ -1,6 +1,8 @@
 package com.cydeo.tests.Day10_uploadAction_jsexecutor;
 
 import com.cydeo.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 public class T3_Hover_Test {
@@ -11,17 +13,20 @@ public class T3_Hover_Test {
 
         Driver.getDriver().get("https://practice.cydeo.com/hovers");
 
-
+        //Locate all images under here:
+        WebElement img1 = Driver.getDriver().findElement(By.xpath("(//img)[1]"));
 
     }
     //2. Hover over to first image
-    //        3. Assert:
+    //3. Assert:
     //a. “name: user1” is displayed
-    //b. “view profile” is displayed 4. Hover over to second image
-             //5. Assert:
+    //b. “view profile” is displayed
+    //4. Hover over to second image
+    //5. Assert:
     //a. “name: user2” is displayed
-    //b. “view profile” is displayed 6. Hover over to third image
-//7. Confirm:
+    //b. “view profile” is displayed //
+    //6. Hover over to third image
+    //7. Confirm:
     //a. “name: user3” is displayed b. “view profile” is displayed
 
 
