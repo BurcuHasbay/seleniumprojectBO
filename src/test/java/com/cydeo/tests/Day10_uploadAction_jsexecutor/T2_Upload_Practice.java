3e13d1b78348cf1951207b1b21b09749a347701a
burcuhasbay <burcu.hasbay@gmail.com>
2023-09-11 18:09:11 +0300

    Upload Practice

---
+++ src/test/java/com/cydeo/tests/Day10_uploadAction_jsexecutor/T2_Upload_Practice.java
@@ -0,0 +1,17 @@
+package com.cydeo.tests.Day10_uploadAction_jsexecutor;
+
+import org.testng.annotations.Test;
+
+public class T2_Upload_Practice {
+
+    @Test
+    public void uploadTest(){
+
+        //TC #2 Upload Test
+        //1. Go to https://practice.cydeo.com/upload
+       // 2. Find some small file from your computer, and get the path of it. 3. Upload the file.
+       // 4. Assert:
+       // -File uploaded text is displayed on the page
+    }
+
+}
