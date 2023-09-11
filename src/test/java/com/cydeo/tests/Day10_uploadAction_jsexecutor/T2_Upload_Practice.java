ad4d6d90342e51704d19c387d24c2f19679324ca
burcuhasbay <burcu.hasbay@gmail.com>
2023-09-11 18:10:23 +0300

    Upload Practice

--- src/test/java/com/cydeo/tests/Day10_uploadAction_jsexecutor/T2_Upload_Practice.java
+++ src/test/java/com/cydeo/tests/Day10_uploadAction_jsexecutor/T2_Upload_Practice.java
@@ -1,5 +1,6 @@
 package com.cydeo.tests.Day10_uploadAction_jsexecutor;
 
+import com.cydeo.utilities.Driver;
 import org.testng.annotations.Test;
 
 public class T2_Upload_Practice {
@@ -9,6 +10,10 @@ public class T2_Upload_Practice {
 
         //TC #2 Upload Test
         //1. Go to https://practice.cydeo.com/upload
+
+        Driver.getDriver().get("https://practice.cydeo.com/upload");
+
+
        // 2. Find some small file from your computer, and get the path of it. 3. Upload the file.
        // 4. Assert:
        // -File uploaded text is displayed on the page
