e9f37f29c2a9daba214af9ee66595f2c66ad29fc
burcuhasbay <burcu.hasbay@gmail.com>
2023-09-12 15:32:41 +0300

    JavaScriptExecutor

---
+++ src/test/java/com/cydeo/tests/Day10_uploadAction_jsexecutor/T6_javascriptExecutor.java
@@ -0,0 +1,33 @@
+package com.cydeo.tests.Day10_uploadAction_jsexecutor;
+
+import com.cydeo.utilities.Driver;
+import org.testng.annotations.Test;
+
+public class T6_javascriptExecutor {
+
+    @Test
+    public void javascriptExecutor_Test(){
+
+        //TC #6: Scroll using JavascriptExecutor
+//1- Open a chrome browser
+//2- Go to: https://practice.cydeo.com/infinite_scroll
+
+        Driver.getDriver().get("https://practice.cydeo.com/infinite_scroll");
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+//3- Use below JavaScript method and scroll
+   // a. 750 pixels down 10 times.
+           // b. 750 pixels up 10 times
+    //JavaScript method to use : window.scrollBy(0,0)
+}
