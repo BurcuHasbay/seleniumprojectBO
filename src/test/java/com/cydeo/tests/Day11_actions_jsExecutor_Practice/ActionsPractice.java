26dd6bda47624e30f2c68bc8b745f93820c0ccb3
burcuhasbay <burcu.hasbay@gmail.com>
2023-09-12 17:15:14 +0300

    Link locator

--- src/test/java/com/cydeo/tests/Day11_actions_jsExecutor_Practice/ActionsPractice.java
+++ src/test/java/com/cydeo/tests/Day11_actions_jsExecutor_Practice/ActionsPractice.java
@@ -1,6 +1,9 @@
 package com.cydeo.tests.Day11_actions_jsExecutor_Practice;
 
 import com.cydeo.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
 public class ActionsPractice {
@@ -14,7 +17,11 @@ public class ActionsPractice {
 
 
         //3- Scroll down to “Powered by CYDEO”
+        //Create object of Actions and pass our "driver" instance
+        Actions actions = new Actions(Driver.getDriver());
 
+        //Locating cydeo link to be able to pass in the actions method
+       WebElement cydeoLink = Driver.getDriver().findElement(By.linkText("CYDEO"));
 
         //4- Scroll using Actions class “moveTo(element)” method
     }
