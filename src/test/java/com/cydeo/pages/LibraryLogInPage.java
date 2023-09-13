bfaa9ba189a5b0209c586e3cc2fc4115e955dbd8
burcuhasbay <burcu.hasbay@gmail.com>
2023-09-13 16:02:08 +0300

    POM-Design Pattern

---
+++ src/test/java/com/cydeo/pages/LibraryLogInPage.java
@@ -0,0 +1,20 @@
+package com.cydeo.pages;
+
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+
+public class LibraryLogInPage {
+
+    //#1 Initialize the driver instance and the object of the class
+    public LibraryLogInPage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+
+
+
+
+
+
+
+}
