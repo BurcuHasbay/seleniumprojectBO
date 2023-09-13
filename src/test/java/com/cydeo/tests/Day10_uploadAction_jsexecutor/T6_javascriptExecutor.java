57121f30115d71b3b3a8938addb5d773f7005e16
burcuhasbay <burcu.hasbay@gmail.com>
2023-09-13 12:03:01 +0300

    for loop

--- src/test/java/com/cydeo/tests/Day10_uploadAction_jsexecutor/T6_javascriptExecutor.java
+++ src/test/java/com/cydeo/tests/Day10_uploadAction_jsexecutor/T6_javascriptExecutor.java
@@ -1,5 +1,6 @@
 package com.cydeo.tests.Day10_uploadAction_jsexecutor;
 
+import com.cydeo.utilities.BrowserUtils;
 import com.cydeo.utilities.Driver;
 import org.openqa.selenium.JavascriptExecutor;
 import org.testng.annotations.Test;
@@ -7,7 +8,7 @@ import org.testng.annotations.Test;
 public class T6_javascriptExecutor {
 
     @Test
-    public void javascriptExecutor_Test(){
+    public void javascriptExecutor_Test() {
 
         //TC #6: Scroll using JavascriptExecutor
 //1- Open a chrome browser
@@ -15,28 +16,22 @@ public class T6_javascriptExecutor {
 
         Driver.getDriver().get("https://practice.cydeo.com/infinite_scroll");
 
-       // ((JavascriptExecutor)Driver.getDriver()).executeScript("");
+        // ((JavascriptExecutor)Driver.getDriver()).executeScript("");
 
         JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
 
         //3- Use below JavaScript method and scroll
-
-        //window.scrollBy(0,0)
-
-
-
-
+        for (int i = 0; i <10 ; i++) {
+            BrowserUtils.sleep(1);
+            js.executeScript("window.scrollBy(0,2500)");
+        }
+        //window.scrollBy(0,750)
 
 
     }
 
 
-
-
-
-
-
-   // a. 750 pixels down 10 times.
-           // b. 750 pixels up 10 times
+    // a. 750 pixels down 10 times.
+    // b. 750 pixels up 10 times
     //JavaScript method to use : window.scrollBy(0,0)
 }
