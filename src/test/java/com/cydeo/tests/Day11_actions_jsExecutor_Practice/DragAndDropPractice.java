aaa596e70a18ac6c2f94f974d27bffddd08db13d
burcuhasbay <burcu.hasbay@gmail.com>
2023-09-13 13:08:01 +0300

    Drag and Drop Practice

--- src/test/java/com/cydeo/tests/Day11_actions_jsExecutor_Practice/DragAndDropPractice.java
+++ src/test/java/com/cydeo/tests/Day11_actions_jsExecutor_Practice/DragAndDropPractice.java
@@ -1,12 +1,14 @@
 package com.cydeo.tests.Day11_actions_jsExecutor_Practice;
 
 import com.cydeo.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 public class DragAndDropPractice {
     /*
    // #: Drag and drop
-//2. Drag and drop the small circle to bigger circle.
+/
 //3. Assert: -Text in big circle changed to: “You did great!”
      */
 
@@ -16,6 +18,21 @@ public class DragAndDropPractice {
 
         //1. Go to https://demos.telerik.com/kendo-ui/dragdrop/index
         Driver.getDriver().get("https://demos.telerik.com/kendo-ui/dragdrop/index");
+
+        //Locate the Smaller Circle fist.
+        WebElement SmallerCircle = Driver.getDriver().findElement(By.id("draggable"));
+        //Locate The Bigger Circle First.
+        WebElement BiggerCircle = Driver.getDriver().findElement(By.id("droptarget"));
+
+        //2. Drag and drop the small circle to bigger circle.
+
+
+
+
+
+
+
+
 
     }
 }
