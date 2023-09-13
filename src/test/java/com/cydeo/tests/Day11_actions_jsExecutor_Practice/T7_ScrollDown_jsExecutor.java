2e36f0ee270babf89f042a9c8ba0f1e9727b5a2d
burcuhasbay <burcu.hasbay@gmail.com>
2023-09-13 12:39:10 +0300

    ScrollDown Practice

--- src/test/java/com/cydeo/tests/Day11_actions_jsExecutor_Practice/T7_ScrollDown_jsExecutor.java
+++ src/test/java/com/cydeo/tests/Day11_actions_jsExecutor_Practice/T7_ScrollDown_jsExecutor.java
@@ -1,6 +1,8 @@
 package com.cydeo.tests.Day11_actions_jsExecutor_Practice;
 
 import com.cydeo.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 public class T7_ScrollDown_jsExecutor {
@@ -9,7 +11,7 @@ public class T7_ScrollDown_jsExecutor {
     /*
    //#7: Scroll using JavascriptExecutor
 
-//3- Scroll down to “Cydeo” link
+
 //4- Scroll up to “Home” link
 //5- Use below provided JS method only
 //JavaScript method to use : arguments[0].scrollIntoView(true)
@@ -20,9 +22,14 @@ public class T7_ScrollDown_jsExecutor {
     @Test
     public void task7_scroll_Test(){
         //1- Open a chrome browser
-//2- Go to: https://practice.cydeo.com/large
-        Driver.getDriver().get("https://practice.cydeo.com/large ");
+        //2- Go to: https://practice.cydeo.com/large
+        Driver.getDriver().get("https://practice.cydeo.com/large");
 
+        //3- Scroll down to “Cydeo” link
+        //Locate the CYDEO link first.
+        WebElement cydeoLink= Driver.getDriver().findElement(By.linkText("CYDEO"));
+        //Locate Home Link first.
+        WebElement homeLink=Driver.getDriver().findElement(By.linkText("Home"));
 
 
     }
