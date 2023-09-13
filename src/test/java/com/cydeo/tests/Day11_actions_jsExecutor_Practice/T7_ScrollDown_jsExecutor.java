6a5274fbed84df53450b3db8b5318807fc6a1fdf
burcuhasbay <burcu.hasbay@gmail.com>
2023-09-13 12:32:14 +0300

    ScrollDown Practice

---
+++ src/test/java/com/cydeo/tests/Day11_actions_jsExecutor_Practice/T7_ScrollDown_jsExecutor.java
@@ -0,0 +1,30 @@
+package com.cydeo.tests.Day11_actions_jsExecutor_Practice;
+
+import com.cydeo.utilities.Driver;
+import org.testng.annotations.Test;
+
+public class T7_ScrollDown_jsExecutor {
+
+
+    /*
+   //#7: Scroll using JavascriptExecutor
+
+//3- Scroll down to “Cydeo” link
+//4- Scroll up to “Home” link
+//5- Use below provided JS method only
+//JavaScript method to use : arguments[0].scrollIntoView(true)
+//Note: You need to locate the links as web elements and pass them as arguments into executeScript() method
+
+     */
+
+    @Test
+    public void task7_scroll_Test(){
+        //1- Open a chrome browser
+//2- Go to: https://practice.cydeo.com/large
+        Driver.getDriver().get("https://practice.cydeo.com/large ");
+
+
+
+    }
+
+}
