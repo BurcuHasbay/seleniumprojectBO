c1f825b4a5ef8df365c71ccb499a2a77e7738ddb
burcuhasbay <burcu.hasbay@gmail.com>
2023-09-14 13:02:29 +0300

    Driver Close

--- src/test/java/com/cydeo/tests/Day12_POM_Design_ExplicitWait/POMPractices.java
+++ src/test/java/com/cydeo/tests/Day12_POM_Design_ExplicitWait/POMPractices.java
@@ -63,7 +63,7 @@ public class POMPractices {
 
 
     @Test
-    public void invalidUsername_or_invalidPassword(){
+    public void Task3_invalidUsername_or_invalidPassword(){
         LibraryLogInPage libraryLogInPage = new LibraryLogInPage();
 
         //3- Enter incorrect username or incorrect password
@@ -79,10 +79,21 @@ public class POMPractices {
 
     }
 
+    @Test
+    public void library_negative_SignIn
+
+    //3- Enter incorrect username or incorrect password
 
 }
 
+/*
+Library negative login
+//1- Open a chrome browser
+//2- Go to: https://library1.cydeo.com
 
+//4- Verify title expected error is displayed:
+//Expected: Sorry, Wrong Email or Password NOTE: FOLLOW POM DESIGN PATTERN
+ */
 
 /*
 #3: Library negative login
