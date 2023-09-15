e427feeece5c5f63cafa5cde4c7f4b89038078d6
burcuhasbay <burcu.hasbay@gmail.com>
2023-09-15 16:15:21 +0300

    Double Click Page

--- src/test/java/com/cydeo/pages/DoubleClickPage.java
+++ src/test/java/com/cydeo/pages/DoubleClickPage.java
@@ -1,6 +1,8 @@
 package com.cydeo.pages;
 
 import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class DoubleClickPage {
@@ -8,8 +10,12 @@ public class DoubleClickPage {
     public DoubleClickPage(){
 
         PageFactory.initElements(Driver.getDriver(),this);
-
-
     }
+
+    @FindBy(id = "demo")
+    public WebElement textToDoubleClick;
+
+
+
 
 }
