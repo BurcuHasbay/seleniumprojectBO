c4446df66bdbb4448658ea6b0a7f1fabd6c6c75f
burcuhasbay <burcu.hasbay@gmail.com>
2023-09-15 12:11:11 +0300

    Dynamic Load Page Constructor

--- src/test/java/com/cydeo/pages/DynamicLoad7Page.java
+++ src/test/java/com/cydeo/pages/DynamicLoad7Page.java
@@ -1,4 +1,14 @@
 package com.cydeo.pages;
 
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+
 public class DynamicLoad7Page {
+
+    //1-Create constructor and instantiate driver and object of the class.
+    public  DynamicLoad7Page(){
+        PageFactory.initElements(Driver.getDriver(),this);
+
+    }
+
 }
