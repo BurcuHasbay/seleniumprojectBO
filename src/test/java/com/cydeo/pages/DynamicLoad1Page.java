20da3df9bf60d3eaa765287648ad3da869c6c476
burcuhasbay <burcu.hasbay@gmail.com>
2023-09-15 13:06:37 +0300

    FindBy Locator

--- src/test/java/com/cydeo/pages/DynamicLoad1Page.java
+++ src/test/java/com/cydeo/pages/DynamicLoad1Page.java
@@ -1,6 +1,8 @@
 package com.cydeo.pages;
 
 import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class DynamicLoad1Page {
@@ -10,6 +12,14 @@ public class DynamicLoad1Page {
     public DynamicLoad1Page(){
         PageFactory.initElements(Driver.getDriver(),this);
     }
+
+    @FindBy(xpath = "//div[@id='start']")
+    public WebElement startButton;
+
+
+
+    @FindBy(xpath = "//input[@type='username']")
+    public WebElement inputUserName;
 
 
 
