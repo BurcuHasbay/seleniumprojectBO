c6595f44eef8cf8a5b7054a734288da97e908577
burcuhasbay <burcu.hasbay@gmail.com>
2023-09-15 17:18:52 +0300

    Dynamic Control Page task1

--- src/test/java/com/cydeo/tests/Day13_review_and_practice/ExplicitWay_Practices.java
+++ src/test/java/com/cydeo/tests/Day13_review_and_practice/ExplicitWay_Practices.java
@@ -2,8 +2,12 @@ package com.cydeo.tests.Day13_review_and_practice;
 
 import com.cydeo.pages.DynamicControlPage;
 import com.cydeo.utilities.Driver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class ExplicitWay_Practices {
 
@@ -18,11 +22,14 @@ public class ExplicitWay_Practices {
 
     @Test
     public void remove_button_test1(){
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
 
         //3- Click to “Remove” button
        dynamicControlPage.removeButton.click();
 
 
+        //4- Wait until “loading bar disappears”
+        wait.until(ExpectedConditions.invisibilityOf(dynamicControlPage.loadingBAR));
 
 
 
@@ -41,7 +48,7 @@ public class ExplicitWay_Practices {
 //1- Open a chrome browser
 //2- Go to: https://practice.cydeo.com/dynamic_controls
 
-//4- Wait until “loading bar disappears”
+
 //5- Verify:
 //a- Checkbox is not displayed
 //b- “It’s gone!” message is displayed.
