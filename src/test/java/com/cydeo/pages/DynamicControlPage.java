f8a2f66e2643eed4c55c39178a43d2e673023991
burcuhasbay <burcu.hasbay@gmail.com>
2023-09-15 17:10:50 +0300

    Dynamic Control Page FindBy

---
+++ src/test/java/com/cydeo/pages/DynamicControlPage.java
@@ -0,0 +1,21 @@
+package com.cydeo.pages;
+
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class DynamicControlPage {
+
+    public DynamicControlPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(xpath = "//div[@id='checkbox']")
+    public WebElement checkBoxButton;
+
+    @FindBy(id = "loading")
+    public WebElement loadingBAR;
+
+
+}
