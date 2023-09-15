d1857569f3d9b7d6668d48d2e489b7668eee7714
burcuhasbay <burcu.hasbay@gmail.com>
2023-09-15 12:39:52 +0300

    Dynamic Load Task

--- src/test/java/com/cydeo/tests/Day12_POM_Design_ExplicitWait/WebDriverWait_Practices.java
+++ src/test/java/com/cydeo/tests/Day12_POM_Design_ExplicitWait/WebDriverWait_Practices.java
@@ -1,7 +1,11 @@
 package com.cydeo.tests.Day12_POM_Design_ExplicitWait;
 
 import com.cydeo.utilities.Driver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class WebDriverWait_Practices {
 
@@ -12,9 +16,13 @@ public class WebDriverWait_Practices {
 
         Driver.getDriver().get("https://practice.cydeo.com/dynamic_loading/7");
 
+        //2. Wait until title is “Dynamic title”
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
 
+        //3.use the "wait" object  to be able to create certain condition to wait
+        wait.until(ExpectedConditions.titleIs("Dynamic title"));
 
-
+        //4. Assert: Message “Done” is displayed.
 
     }
 
@@ -22,7 +30,8 @@ public class WebDriverWait_Practices {
 /*
 
 
-//2. Wait until title is “Dynamic title”
-//3. Assert: Message “Done” is displayed. 4. Assert: Image is displayed.
+
+
+//4. Assert: Image is displayed.
 //Note: Follow POM
  */
