7dea3652d75a9c8a14a1228148be70257f3735c2
burcuhasbay <burcu.hasbay@gmail.com>
2023-09-15 14:32:24 +0300

    Asserting. is displayed

--- src/test/java/com/cydeo/tests/Day12_POM_Design_ExplicitWait/WebDriverWait_Practices.java
+++ src/test/java/com/cydeo/tests/Day12_POM_Design_ExplicitWait/WebDriverWait_Practices.java
@@ -74,8 +74,7 @@ public class WebDriverWait_Practices {
         dynamicLoad1Page.submitButton.click();
 
         //8. Assert “Your password is invalid!” text is displayed.
-
-
+        Assert.assertTrue(dynamicLoad1Page.errorMessage.isDisplayed());
 
     }
 
