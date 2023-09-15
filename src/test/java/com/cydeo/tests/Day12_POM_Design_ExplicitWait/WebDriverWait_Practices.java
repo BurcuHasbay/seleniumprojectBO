70c1e6504ac8333c1d448d824a06971e4300ac1d
burcuhasbay <burcu.hasbay@gmail.com>
2023-09-15 12:57:05 +0300

    Dynamic Load 1 Test

--- src/test/java/com/cydeo/tests/Day12_POM_Design_ExplicitWait/WebDriverWait_Practices.java
+++ src/test/java/com/cydeo/tests/Day12_POM_Design_ExplicitWait/WebDriverWait_Practices.java
@@ -43,11 +43,27 @@ public class WebDriverWait_Practices {
     @Test
     public void dynamically_load_1_Test(){
 
+        //#5 : Dynamically Loaded Page Elements 1
+        //1. Go to https://practice.cydeo.com/dynamic_loading/1
+        Driver.getDriver().get("https://practice.cydeo.com/dynamic_loading/1");
+
+
     }
 
 }
 
+/*
 
+
+//2. Click to start
+//3. Wait until loading bar disappears
+//4. Assert username inputbox is displayed
+//5. Enter username: tomsmith
+//6. Enter password: incorrectpassword
+//7. Click to Submit button
+//8. Assert “Your password is invalid!” text is displayed.
+//Note: Follow POM Design Pattern
+ */
 
 
 
