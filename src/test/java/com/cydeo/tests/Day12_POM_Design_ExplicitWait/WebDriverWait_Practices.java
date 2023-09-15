bdcc9a83096f092e7165c5ac1609a047add86ded
burcuhasbay <burcu.hasbay@gmail.com>
2023-09-15 12:20:06 +0300

    Dynamic Load Task

---
+++ src/test/java/com/cydeo/tests/Day12_POM_Design_ExplicitWait/WebDriverWait_Practices.java
@@ -0,0 +1,26 @@
+package com.cydeo.tests.Day12_POM_Design_ExplicitWait;
+
+import org.testng.annotations.Test;
+
+public class WebDriverWait_Practices {
+
+
+    @Test
+    public void dynamic_load_7_Test(){
+    //1. Go to https://practice.cydeo.com/dynamic_loading/7
+
+
+
+
+
+
+    }
+
+}
+/*
+
+
+//2. Wait until title is “Dynamic title”
+//3. Assert: Message “Done” is displayed. 4. Assert: Image is displayed.
+//Note: Follow POM
+ */
