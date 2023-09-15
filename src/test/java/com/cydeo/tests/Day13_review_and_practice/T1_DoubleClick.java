bdb7667cbb887f1352fc3c624d2806b03b71295c
burcuhasbay <burcu.hasbay@gmail.com>
2023-09-15 16:22:26 +0300

    iframe

--- src/test/java/com/cydeo/tests/Day13_review_and_practice/T1_DoubleClick.java
+++ src/test/java/com/cydeo/tests/Day13_review_and_practice/T1_DoubleClick.java
@@ -1,5 +1,6 @@
 package com.cydeo.tests.Day13_review_and_practice;
 
+import com.cydeo.pages.DoubleClickPage;
 import com.cydeo.utilities.Driver;
 import org.testng.annotations.Test;
 
@@ -12,10 +13,12 @@ public class T1_DoubleClick {
         Driver.getDriver().get("//https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_ev_ondblclick2");
 
         //2. Switch to i frame.
+        //An inline frame (iframe) is a HTML element that loads another HTML page within the document.
+        Driver.getDriver().switchTo().frame(0);
 
-
-
-
+        //3. Double click on the text “Double- click me to change my text color.”
+        DoubleClickPage doubleClickPage = new DoubleClickPage();
+        doubleClickPage.textToDoubleClick.click();
 
 
     }
@@ -27,7 +30,7 @@ public class T1_DoubleClick {
 //#1: Double Click Test 1. Goto
 //https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_ev_ondblclick2
 
-//3. Double click on the text “Double- click me to change my text color.”
+
 //4. Assert:Text’s“style”attribute value contains“red”.
 //NOTE: FOLLOW POM
  */
