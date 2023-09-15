8b8547260c9c0f6dae407aa941d104e8884604e0
burcuhasbay <burcu.hasbay@gmail.com>
2023-09-15 12:15:07 +0300

    'Done' message Locator using FindBy

--- src/test/java/com/cydeo/pages/DynamicLoad7Page.java
+++ src/test/java/com/cydeo/pages/DynamicLoad7Page.java
@@ -1,6 +1,8 @@
 package com.cydeo.pages;
 
 import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class DynamicLoad7Page {
@@ -10,5 +12,13 @@ public class DynamicLoad7Page {
         PageFactory.initElements(Driver.getDriver(),this);
 
     }
+
+    @FindBy(xpath = "//strong[.='Done!']")
+    public WebElement doneMessage;
+
+
+
+
+
 
 }
